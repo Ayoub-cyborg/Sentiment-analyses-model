71e0ffc4501e0e8fe25d12064d1b71b66478bb7d
unknown <boumghaderayoub15@gmail.com>
2023-06-02 21:03:38 +0100

    deploy the model in the streamlit app

--- app.py
+++ app.py
@@ -1,40 +1,87 @@
 import streamlit as st
+import altair as alt
+import pandas as pd
+import numpy as np
+import joblib
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Load the model and tokenizer
+#pipe_lr = joblib.load("./models/emotion_classifier_pipe_lr_1.0.1.pkl", "rb")
+pipe_lr = tf.keras.models.load_model('./models/saved_model/my_model')
+tokenizer = joblib.load("./models/tokenizer1.0.1.pkl")
+
+sentiment_labels = ['negative', 'positive']
+
+# Functions
+def predict_emotions(docx):
+    sequences = tokenizer.texts_to_sequences([docx])
+    padded_sequences = pad_sequences(sequences, maxlen=100)
+    results = pipe_lr.predict(padded_sequences)
+    # Check if predictions exist
+    if results.shape[0] > 0:
+    # Classify the sentiment based on the prediction probabilities
+         classified_sentiment = sentiment_labels[np.argmax(results[0])]
+    else:
+    # Unable to classify se ntiment
+         classified_sentiment = "Unknown"
+    return classified_sentiment
+
+def get_prediction_proba(docx):
+    sequences = tokenizer.texts_to_sequences([docx])
+    padded_sequences = pad_sequences(sequences, maxlen=100)
+    results = pipe_lr.predict(padded_sequences)
+    class_probabilities = results
+    return class_probabilities
+
+emotions_emoji_dict = {"positive": "🤗", "negative": "😠"}
+
+
+
+
+# Main function
 def main():
-	st.title("Sentiment Analysis NLP App")
-	st.subheader("Streamlit Projects")
+    st.title("Emotion Classifier App")
+    menu = ["Home", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
 
-	menu = ["Home","About"]
-	choice = st.sidebar.selectbox("Menu",menu)
+    if choice == "Home":
+        st.subheader("_Home-Emotion In Text_")
 
-	if choice == "Home":
-		st.subheader("Home")
-		with st.form(key='nlpForm'):
-			raw_text = st.text_area("Enter Text Here")
-			submit_button = st.form_submit_button(label='Analyze')
+        with st.form(key="emotion_clf_form"):
+            raw_text = st.text_area("Type Here")
+            submit_text = st.form_submit_button(label="Analyze")
 
-		# layout
-		col1,col2 = st.columns(2)
-		if submit_button:
+        if submit_text:
+            col1, col2 = st.columns(2)
 
-			with col1:
-				pass
+            # Apply functions here
+            prediction = predict_emotions(raw_text)
+            emoji_icon = emotions_emoji_dict[prediction]
+            probability = get_prediction_proba(raw_text)
 
-				
+            with col1:
+                st.success("Original Text")
+                st.write(raw_text)
+                st.success("Prediction")
+                st.write("{}: {}".format(prediction, emoji_icon))
+            with col2:
+               
+                st.success("Prediction Probability")
+                #st.write(probability)
+                class_labels = ['negative','positive'] 
+                proba_df = pd.DataFrame(probability,columns=class_labels)
+                st.write(proba_df.T)
+                proba_df_clean = proba_df.T.reset_index()
+                proba_df_clean.columns = ['emotions','probability']
 
-
-
-			with col2:
-				st.info("Token Sentiment")
-
-				pass
-
-
-
-
-
-	else:
-		st.subheader("About")
+                fig = alt.Chart(proba_df_clean).mark_bar().encode(x="emotions",y=alt.Y("probability", axis=alt.Axis(format='%')),color='emotions')
+                st.altair_chart(fig,use_container_width=True)
+    else:
+        st.subheader("About")
 
 
 if __name__ == '__main__':
-	main()
+    main()
